949f526b6a999c56733010225d6388848d9721d4
snoepjeslik <115473064+snoepjeslik@users.noreply.github.com>
2022-10-10 14:31:48 +0200

    je moeder
    je dode oma

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.body.style.cssText = 'padding: 0px; margin: 0px; overflow-x: hidden;';
 
 var parentContainer = document.createElement('div'); 
 document.body.appendChild(parentContainer);
-parentContainer.style.cssText = 'display: flex;';
+parentContainer.style.cssText = 'display: flex; background-color:blue;';
 parentContainer.style.height = window.innerHeight + 'px';
 parentContainer.style.width = window.innerWidth + 'px';
 parentContainer.style.backgroundColor = 'white';
